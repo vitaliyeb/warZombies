42781d36e774aa0c3fb20be12b6ff2ffb94cc3a1
Vitaliy Burdin <vlvlv.vovov@bk.ru>
2022-01-11 15:10:55 +0500

    add cra

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, {useEffect} from "react";
+import Web3 from "web3";
+
+const App = () => {
+
+  useEffect(() => {
+    console.log(Web3);
+  });
+
+  return <div>
+    test
+  </div>
+}
+
+
+export default App;
